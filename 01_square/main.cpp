2873b696ebdfe8259fecca2b03580c2fdd91d6dc
adv-dev-22 <a2nced.c0mputin9.lms.17551@yandex.ru>
2021-05-02 15:34:35 +0300

    Introduce base program

---
+++ 01_square/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(int argc, char * args [])
+{
+    const int n = 25;
+
+    for (int x = 0; x < n; ++x) {
+        for (int y = 0; y < n; ++y)
+        {
+            if (x < y)
+                std::cout << "# ";
+            else
+                std::cout << ". ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
+
+
+//
